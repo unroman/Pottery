5910ef743e8fa6f44544cbf6ae8fa82702eebf1a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-20 10:39:07 +0200

    Use separate random for PotBlockRenderer #7

--- src/main/java/com/supermartijn642/pottery/content/PotBlockRenderer.java
+++ src/main/java/com/supermartijn642/pottery/content/PotBlockRenderer.java
@@ -5,12 +5,12 @@ import com.mojang.math.Axis;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.render.CustomBlockEntityRenderer;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.entity.DecoratedPotBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -18,6 +18,8 @@ import net.minecraft.world.level.block.state.BlockState;
  * Created 27/12/2023 by SuperMartijn642
  */
 public class PotBlockRenderer implements CustomBlockEntityRenderer<PotBlockEntity> {
+    private static final RandomSource RANDOM_SOURCE = RandomSource.create();
+
     @Override
     public void render(PotBlockEntity entity, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay){
         poseStack.pushPose();
@@ -50,7 +52,7 @@ public class PotBlockRenderer implements CustomBlockEntityRenderer<PotBlockEntit
         BlockRenderDispatcher blockRenderer = ClientUtils.getBlockRenderer();
         BlockState state = entity.getBlockState();
         BakedModel model = blockRenderer.getBlockModel(state);
-        blockRenderer.getModelRenderer().tesselateBlock(entity.getLevel(), model, state, entity.getBlockPos(), poseStack, bufferSource.getBuffer(ItemBlockRenderTypes.getChunkRenderType(state)), cullFaces, entity.getLevel().random, combinedLight, combinedOverlay);
+        blockRenderer.getModelRenderer().tesselateBlock(entity.getLevel(), model, state, entity.getBlockPos(), poseStack, bufferSource.getBuffer(ItemBlockRenderTypes.getChunkRenderType(state)), cullFaces, RANDOM_SOURCE, state.getSeed(entity.getBlockPos()), combinedOverlay);
 
         poseStack.popPose();
     }
